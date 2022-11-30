bfc2d5a427dc246b6a44eaaaebbd38c9cb850261
Gabriel MP <contato.gabrielmartinspadoin@gmail.com>
2022-11-30 09:44:45 -0300

    limpando os intervalos

--- src/components/TrackerForm.vue
+++ src/components/TrackerForm.vue
@@ -33,7 +33,8 @@ export default defineComponent({
   name: 'TrackerForm',
   data () {
     return {
-      tempoEmSegundos: 0
+      tempoEmSegundos: 0,
+      cronometro: 0
     }
   },
   computed: {
@@ -43,12 +44,12 @@ export default defineComponent({
   },
   methods: {
     iniciar(){
-      setInterval(() => {
+      this.cronometro = setInterval(() => {
         this.tempoEmSegundos += 1
       }, 1000)
     },
     finalizar(){
-      console.log('finalizando');
+      clearInterval(this.cronometro)
     }
   }
 })
