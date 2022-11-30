ac1316a1e34f39df0ff87037d38dd1e36c120e2f
Gabriel MP <contato.gabrielmartinspadoin@gmail.com>
2022-11-30 16:18:12 -0300

    eventos e props

--- src/components/TrackerForm.vue
+++ src/components/TrackerForm.vue
@@ -2,10 +2,15 @@
   <div class="box">
     <div class="columns">
       <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
-        <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" />
+        <input 
+          type="text"
+          class="input"
+          placeholder="Qual tarefa você deseja iniciar?"
+          v-model="descricao"
+        />
       </div>
       <div class="column">
-        <TrackerTimerController/>
+        <TrackerTimerController @whenFinished="finalizarTarefa"/>
       </div>
     </div>
   </div>
@@ -19,6 +24,18 @@ export default defineComponent({
   components: {
     TrackerTimerController
   },
+  data () {
+    return {
+      descricao: ''
+    }
+  },
+  methods: {
+    finalizarTarefa (tempoEmSegundos: number): void {
+      console.log('tempo da tarefa: ', tempoEmSegundos)
+      console.log('descricao: ', this.descricao)
+      this.descricao = ''
+    }
+  }
 })
 </script>
 
