4cad8182fc56e168dcb9ba0eed3d85160218c979
Gabriel MP <contato.gabrielmartinspadoin@gmail.com>
2022-11-30 10:47:14 -0300

    refatorando o formulario

--- src/components/TrackerForm.vue
+++ src/components/TrackerForm.vue
@@ -6,9 +6,7 @@
       </div>
       <div class="column">
         <div class="is-flex is-align-items-center is-justify-content-space-between">
-          <section>
-            <strong>{{ tempoDecorrido }}</strong>
-          </section>
+          <TrackerTimer :tempoEmSegundos="tempoEmSegundos" />
           <button class="button" @click="iniciar">
             <span class="icon">
               <i class="fas fa-play"></i>
@@ -29,17 +27,16 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import TrackerTimer from './TrackerTimer.vue'
 export default defineComponent({
   name: 'TrackerForm',
+  components: {
+    TrackerTimer
+  },
   data () {
     return {
       tempoEmSegundos: 0,
       cronometro: 0
-    }
-  },
-  computed: {
-    tempoDecorrido (): string {
-      return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8)
     }
   },
   methods: {
